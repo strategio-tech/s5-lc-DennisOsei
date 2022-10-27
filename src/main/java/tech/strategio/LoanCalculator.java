0e1de6b92e5033c1da8ece64282ab853713bba75
Dennis <dennisoseitimss1@gmail.com>
2022-10-27 11:13:27 -0400

    added solution

--- src/main/java/tech/strategio/LoanCalculator.java
+++ src/main/java/tech/strategio/LoanCalculator.java
@@ -5,15 +5,17 @@ import java.util.Scanner;
 public class LoanCalculator {
 
     /**
-     * TODO: finish out this JavaDoc comment block.
-     * FIXME: WHAT DOES THIS METHOD DO?
-     * 
-     * @param ???
-     * @return ???
+     * First we run a for loop 3 times, which stands for the number of months
+     * In each iteration we are finding the 10 % percent of the amount owed and then
+     * subtracting it from the current total amount owed.
+     * @param int
+     * @return int
      */
     static int getRemainingAmountIn3Months(int amount) {
-        // TODO: Rewrite this method
-        return -1;
+        for (int x = 0; x < 3; x++) {
+            amount -= ((amount * 10) / 100);
+        }
+        return amount;
     }
 
     public static void main(String[] args) {
